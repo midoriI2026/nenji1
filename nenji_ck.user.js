4029fc3742a9a8de38735955da487a40b132521a
midoriI2026 <106957602+midoriI2026@users.noreply.github.com>
2026-04-29 13:02:37 +0900

    Create nenji_ck.user.js

---
+++ nenji_ck.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         ★年次カスタム
+// @namespace    http://tampermonkey.net/
+// @version      8.0
+// @match        https://midorinet-iwate.cybozu.com/o/ag.cgi?page=DBRecord&did=35*
+// @match        https://midorinet-iwate.cybozu.com/o/ag.cgi?page=DBForm&did=35*
+// @match        https://midorinet-iwate.cybozu.com/o/ag.cgi?Page=DBSearchResult&DID=35*
+// @run-at       document-end
+// @require      https://raw.githubusercontent.com/midoriI2026/nenji1/main/nenji2_ck.js?v=1.0
+// @grant        none
+// ==/UserScript==
