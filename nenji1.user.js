704636724799383e475e0422912cc51053e1c0a6
midoriI2026 <106957602+midoriI2026@users.noreply.github.com>
2026-04-29 12:28:53 +0900

    Update nenji1.user.js

--- nenji1.user.js
+++ nenji1.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name         ★年次CopyPaste（再利用ボタン方式・安定版）
+// @name         ★年次カスタム
 // @namespace    http://tampermonkey.net/
 // @version      8.0
 // @match        https://midorinet-iwate.cybozu.com/o/ag.cgi?page=DBRecord&did=35*
 // @match        https://midorinet-iwate.cybozu.com/o/ag.cgi?page=DBForm&did=35*
 // @run-at       document-end
 // @require      https://raw.githubusercontent.com/midoriI2026/nenji1/main/nenji1.js?v=1.0
+// @require      https://raw.githubusercontent.com/midoriI2026/nenji1/main/nenji2_ck.js?v=1.0
 // @grant        none
 // ==/UserScript==
