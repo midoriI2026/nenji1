db8ff3c860e5e55e02a5252e4814507aacc375fa
midoriI2026 <106957602+midoriI2026@users.noreply.github.com>
2026-04-29 11:39:09 +0900

    Create nenji1.user.js

---
+++ nenji1.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         ★年次CopyPaste（再利用ボタン方式・安定版）
+// @namespace    http://tampermonkey.net/
+// @version      8.0
+// @match        https://midorinet-iwate.cybozu.com/o/ag.cgi?page=DBRecord&did=35*
+// @match        https://midorinet-iwate.cybozu.com/o/ag.cgi?page=DBForm&did=35*
+// @run-at       document-end
+// @require      https://raw.githubusercontent.com/midoriI2026/nenji1/refs/heads/main/nenji1.js
+// @grant        none
+// ==/UserScript==
